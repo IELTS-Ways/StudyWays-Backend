1b38d9f80c9958379258c576535aa94dba9aace3
Armin Mozafary <armin.arm90@gmail.com>
2024-10-21 10:55:55 +0330

    contact us configs

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=254,blank=False)
+    description = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} : {self.description}'
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Contact
+from .serializers import ContactSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class ContactUsView(APIView):
+    serializer_class = ContactSerializer
+    
+    def post(self, *args, **kwargs):
+        data = self.request.data
+        serializer = self.serializer_class(data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
