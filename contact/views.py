a4ecefeab26c0fcabd996c18dba44b369ddbc2ef
Armin Mozafary <armin.arm90@gmail.com>
2024-10-21 22:18:24 +0330

    contact models and views Updated

--- contact/views.py
+++ contact/views.py
@@ -4,11 +4,13 @@ from .serializers import ContactSerializer, ApplicationSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class ContactUsView(APIView):
     serializer_class = ContactSerializer
-    
+    permission_classes = [AllowAny]
+
     def post(self, *args, **kwargs):
         data = self.request.data
         serializer = self.serializer_class(data=data, partial=True)
@@ -19,7 +21,8 @@ class ContactUsView(APIView):
     
 class ApplicationFormView(APIView):
     serializer_class = ApplicationSerializer
-    
+    permission_classes = [AllowAny]
+
     def post(self, *args, **kwargs):
         data = self.request.data
         serializer = self.serializer_class(data=data, partial=True)
