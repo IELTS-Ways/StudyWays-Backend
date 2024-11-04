93048c9cb4c9ae9e6801a15bd36aa72b7c4af541
Fan Wang <nimadorostkar97@gmail.com>
2024-11-04 10:18:25 +0330

    with_remaining_days

--- subscription/models.py
+++ subscription/models.py
@@ -18,8 +18,8 @@ class SingletonModel(models.Model):
 class SubscriptionManager(models.Manager):
     def with_remaining_days(self):
         today = date_time.now().date()
-        elapsed_days = (today - self.created_at).days
-        remaining_expr = ExpressionWrapper(self.day_period - elapsed_days,output_field=fields.IntegerField())
+        elapsed_days = (today - F('created_at')).days
+        remaining_expr = ExpressionWrapper(F('day_period') - elapsed_days,output_field=fields.IntegerField())
         return self.get_queryset().annotate(remaining_days=remaining_expr)
 
 
