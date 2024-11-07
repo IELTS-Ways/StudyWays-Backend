a4c0407e8cc64b27665b444f424f0ece213c8970
Fan Wang <nimadorostkar97@gmail.com>
2024-11-07 10:43:04 +0330

    create_or_update_user_profile

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from accounts.models import User, StudentProfile, InstituteProfile, FreelanceProfile, MarketerPanel
 
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if instance.user_type == "student":
