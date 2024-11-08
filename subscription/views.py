5931b40f53357d588be5542c6c9360f9fab10b7a
Fan Wang <nimadorostkar97@gmail.com>
2024-11-08 10:57:11 +0330

    debug AddSubPay comitions

--- subscription/views.py
+++ subscription/views.py
@@ -96,8 +96,8 @@ class AddSubPay(APIView):
                 studyways_price = 0
                 ZP_MERCHANT_ID = default_price.ZP_MERCHANT_ID
                 appor_percent = default_price.freelance_apportionment_percentage
-                apportionment = sub.price * appor_percent
-                freelance_price = sub.price - float(apportionment)          #share with freelance
+                freelance_price = sub.price * appor_percent         #share with freelance
+                apportionment = sub.price - float(freelance_price)
                 sub.freelance_price = freelance_price
                 inviter_price = apportionment * sales_percentage     #share with inviter
 
