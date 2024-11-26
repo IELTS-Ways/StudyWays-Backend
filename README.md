ef6b6d526b586e7b22bcce72eaef6f5eec0086e8
Fan Wang <nimadorostkar97@gmail.com>
2024-11-26 10:01:42 +0330

    update

--- README.md
+++ README.md
@@ -50,5 +50,3 @@ docker exec -it container_id python manage.py createsuperuser
 ```bash
 docker-compose up -d --build
 ```
-
-
--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ContactUsView,ApplicationFormView
 
+
 urlpatterns = [
     path('contact-us/', ContactUsView.as_view(), name='contact-us'),
     path('application/', ApplicationFormView.as_view(), name='application'),
