28bd58631abba471f1796236989ba72938629d21
Armin Mozafary <armin.arm90@gmail.com>
2024-11-27 10:24:28 +0330

    institute wallet api

--- accounts/serializers/institute.py
+++ accounts/serializers/institute.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
-from accounts.models.institute_profile import InstituteProfile
+from accounts.models.institute_profile import InstituteProfile, InstituteWallet
 
 
 class InstituteSerializer(serializers.ModelSerializer):
     class Meta:
         model = InstituteProfile
         fields = "__all__"
+        
+        
+class InstituteWalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InstituteWallet
+        fields = ("balance", "updated_at")
 
