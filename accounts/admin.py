e5c5fd5df97df33ddc270d82cc0f81a2d1fcf5c6
Armin Mozafary <armin.arm90@gmail.com>
2024-12-10 17:02:16 +0330

    updating admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,25 +6,33 @@ from import_export.admin import ImportExportModelAdmin
 
 
 class UserAdmin(ImportExportModelAdmin):
-    list_display = ('phone_number','user_type','email','id','is_profile_fill', 'created_at')
+    list_display = ('phone_number','user_type','email','id', 'first_name', 'last_name', 'is_profile_fill', 'created_at')
     list_filter = ("user_type", "confirmed", "created_at")
     search_fields = ['phone_number', 'first_name', 'last_name', 'email', 'username']
 admin.site.register(User, UserAdmin)
 
 class StudentProfileAdmin(ImportExportModelAdmin):
-    list_display = ("user", "institute", "is_IELTS_student")
+    list_display = ("user", "institute", "freelance", "is_IELTS_student", "english_level")
+    list_filter = ("english_level", "gender", "is_IELTS_student")
+    search_fields = ['cart_number', 'shaba', 'description', 'institute', 'freelance']
 admin.site.register(StudentProfile, StudentProfileAdmin)
 
 class InstituteProfileAdmin(ImportExportModelAdmin):
     list_display = ("user", "school_name", "username")
+    list_filter = ("occupancy_type",)
+    search_fields = ['address', 'postal_code', 'tax_case_number', 'description', 'school_address']
 admin.site.register(InstituteProfile, InstituteProfileAdmin)
 
 class FreelanceProfileAdmin(ImportExportModelAdmin):
     list_display = ("user", "school_name", "username")
+    list_filter = ("occupancy_type",)
+    search_fields = ['address', 'postal_code', 'cart_number', 'description', 'shaba']
 admin.site.register(FreelanceProfile, FreelanceProfileAdmin)
 
 class MarketerPanelAdmin(ImportExportModelAdmin):
     list_display = ("user", "province")
+    list_filter = ("occupancy_type",)
+    search_fields = ['address', 'postal_code', 'cart_number', 'description', 'shaba']
 admin.site.register(MarketerPanel, MarketerPanelAdmin)
 
 class MarketerWalletAdmin(ImportExportModelAdmin):
--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Contact, Application
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Contact)
-admin.site.register(Application)
+
+class ContactAdmin(ImportExportModelAdmin):
+    list_display = ('title', 'name', 'email')
+    list_filter = ("created_at",)
+    search_fields = ['title', 'name', 'email', 'description']
+admin.site.register(Contact, ContactAdmin)
+
+
+class ApplicationAdmin(ImportExportModelAdmin):
+    list_display = ('full_name', 'phone_number', 'email')
+    list_filter = ("location",)
+    search_fields = ['full_name', 'phone_number', 'email', 'question', 'location']
+admin.site.register(Application, ApplicationAdmin)
