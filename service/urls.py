2728e4cf0c5fb6bf049331f13fe432809ca2deb2
Fan Wang <nimadorostkar97@gmail.com>
2024-12-16 17:08:20 +0330

    debug link in service

--- service/urls.py
+++ service/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("services", Services.as_view(), name="services"),
     path("service-item/<int:id>/",ServicesItem.as_view(),name="service-item"),
     path("service-correction/<int:id>/",ServicesCorrection.as_view(),name="service-correction"),
-    path("service-correction-v2/<int:id>/",ServicesCorrectionV2.as_view(),name="service-correction_v2"),
+    path("service-correction-v2/<int:id>/",ServicesCorrectionV2.as_view(),name="service-correction-v2"),
     path("service-correction-ai/<int:id>/",ServicesCorrectionAI.as_view(),name="service-correction-ai"),
     path("feedback", FeedbackView.as_view(), name="feedback"),
     # 
