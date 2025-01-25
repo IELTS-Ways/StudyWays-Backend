ac6ad2a7a310149e9883dc286e353f77296a6627
Armin Mozafary <armin.arm90@gmail.com>
2025-01-25 12:12:04 +0330

    sub debug

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,23 +1,30 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from accounts.models import User, StudentProfile, InstituteProfile, FreelanceProfile, MarketerPanel
+from accounts.models import User, StudentProfile, InstituteProfile, FreelanceProfile, MarketerPanel,FreelanceWallet, MarketerWallet, InstituteWallet, StudentWallet
 
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if instance.user_type == "student":
-        StudentProfile.objects.get_or_create(user=instance)
+        profile, _ = StudentProfile.objects.get_or_create(user=instance)
         if hasattr(instance, 'studentprofile'):
             instance.studentprofile.save()
+        StudentWallet.objects.get_or_create(user=profile)
+
     elif instance.user_type == "institute":
-        InstituteProfile.objects.get_or_create(user=instance)
+        profile, _ = InstituteProfile.objects.get_or_create(user=instance)
         if hasattr(instance, 'instituteprofile'):
             instance.instituteprofile.save()
+        InstituteWallet.objects.get_or_create(user=profile)
+
     elif instance.user_type == "freelance":
-        FreelanceProfile.objects.get_or_create(user=instance)
+        profile, _ = FreelanceProfile.objects.get_or_create(user=instance)
         if hasattr(instance, 'freelanceprofile'):
             instance.freelanceprofile.save()
+        FreelanceWallet.objects.get_or_create(user=profile)
+
     elif instance.user_type == "marketer":
-        MarketerPanel.objects.get_or_create(user=instance)
-        if hasattr(instance, 'marketerprofile'):
-            instance.marketerprofile.save()
+        profile, _ = MarketerPanel.objects.get_or_create(user=instance)
+        if hasattr(instance, 'MarketerPanel'):
+            instance.MarketerPanel.save()
+        MarketerWallet.objects.get_or_create(user=profile)
